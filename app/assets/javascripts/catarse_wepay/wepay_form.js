85ef83a23c5a1a9bc1d4ff6bd718633402edfc37
Mark Aufdencamp <Mark@Aufdencamp.com>
2016-07-27 13:35:20 -0400

    Added wepay_form and user_document js.

---
+++ app/assets/javascripts/catarse_wepay/wepay_form.js
@@ -0,0 +1,19 @@
+CATARSE.UserForm = CATARSE.WePayUserDocument.extend({
+  el: '#catarse_wepay_form',
+
+  events: {
+    'click input[type=submit]': 'onSubmitToWePay',
+    'keyup #user_document' : 'onUserDocumentKeyup'
+  },
+
+  activate: function() {
+    this.loader = $('.loader');
+    this.parent.contributionId = $('input#contribution_id').val();
+    this.parent.projectId = $('input#project_id').val();
+  },
+
+  onSubmitToWePay: function(e) {
+    $(e.currentTarget).hide();
+    this.loader.show();
+  }
+});
